f8cce7f429668df586996ecac2b1ad63771f1e76
atc <birdhive@gmail.com>
2024-06-10 12:23:47 -0400

    further revision

--- includes/acf-settings-page.php
+++ includes/acf-settings-page.php
@@ -17,7 +17,7 @@ function whx4_register_options_page() {
 	// Add the top-level page
 	acf_add_options_page(
 		array(
-			'page_title' => 'WHX4 Settings',
+			'page_title' => 'WHx4 Settings',
 			'menu_slug'  => 'whx4_settings',
 			'redirect'   => false,
 		)
