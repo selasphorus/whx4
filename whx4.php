8bbabf68484e3598a5fcc3c260aba5641e436e07
atc <birdhive@gmail.com>
2025-04-03 20:56:31 -0400

    Functions removed to class files

--- whx4.php
+++ whx4.php
@@ -19,7 +19,6 @@
 // v2 OOP version WIP
 
 if ( !defined( 'ABSPATH' ) ) exit;
-//defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 // Make sure we don't expose any info if called directly
 if ( !function_exists( 'add_action' ) ) {
@@ -27,13 +26,13 @@ if ( !function_exists( 'add_action' ) ) {
 	exit;
 }
 
-/* WIP >> OOP
+// WIP >> OOP
 
-// Via Composer
-require_once __DIR__ . '/vendor/autoload.php';
-//require 'vendor/autoload.php'
+// Autoload Classes via Composer
+require_once __DIR__ . '/vendor/autoload.php'; //require 'vendor/autoload.php'
 $plugin = new WHx4\Core\Plugin();
 
+/* 
 -- OR --
 
 // Via Autoloader Class
@@ -43,25 +42,15 @@ WHx4_Autoloader::register();
 // Found at /WHx4/Plugin.php
 $plugin = new WHx4_Plugin();
 
----
+-- OR --
 
 use atc\WHx4\Plugin;
-
 Plugin::run( entry_point: __FILE__ );
-
 */
 
-/* ***** TODO: Move all of the following away into classes ***** */
+/* ***** TODO/WIP: Move all of the following away into classes ***** */
 
-// Define our handy constants.
-define( 'WHX4_VERSION', '0.2.0' );
-define( 'WHX4_PLUGIN_DIR', __DIR__ );
-define( 'WHX4_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-define( 'WHX4_PLUGIN_BLOCKS', WHX4_PLUGIN_DIR . '/blocks/' );
-
-/* +~+~+ *** +~+~+ */
-
-// Function to check for dev/admin user
+// Function to check for main dev/admin user
 function whx4_queenbee() {
 	$current_user = wp_get_current_user();
 	$username = $current_user->user_login;
@@ -95,95 +84,9 @@ require 'includes/template-tags.php';
 require 'includes/acf-field-groups.php';
 
 // Post types, taxonomies, field groups
-require 'includes/cpts.php';
+//require 'includes/cpts.php'; // Deprecated -- now handled via OOP
 
-// Load custom post types
-//require 'posttypes.php';
 
-// Load custom taxonomies
-//require 'taxonomies.php';
-
-/**
- * Enqueue scripts and styles
- */
-add_action( 'wp_enqueue_scripts', 'whx4_scripts_method' );
-function whx4_scripts_method() {
-    
-    //global $current_user;
-    //$current_user = wp_get_current_user();
-    
-    $fpath = WP_PLUGIN_DIR . '/whx4/css/whx4.css';
-    if (file_exists($fpath)) { $ver = filemtime($fpath); } else { $ver = "240823"; }  
-    wp_enqueue_style( 'whx4-style', plugins_url( 'css/whx4.css', __FILE__ ), $ver );
-    
-    /*$fpath = WP_PLUGIN_DIR . '/whx4/js/whx4.js';
-    if (file_exists($fpath)) { $ver = filemtime($fpath); } else { $ver = date('Ymd.hi'); }
-    wp_enqueue_script( 'whx4', plugins_url( 'js/whx4.js', __FILE__ ), array( 'jquery-ui-dialog' ), $ver  );
-    wp_localize_script( 'whx4', 'theUser', array (
-        'username' => $current_user->user_login,
-    ) );*/
-    
-}
-
-/* +~+~+ Optional Modules +~+~+ */
-
-// Get plugin options -- WIP
-$options = get_option( 'whx4_settings' );
-if ( get_field('whx4_active_modules', 'option') ) { $active_modules = get_field('whx4_active_modules', 'option'); } else { $active_modules = array(); }
-//if ( isset($options['whx4_active_modules']) ) { $active_modules = $options['whx4_active_modules']; } else { $active_modules = array(); }
-foreach ( $active_modules as $module ) {
-    
-    $sub_modules = array();
-    // Add module options page for adding featured image, page-top content, &c.
-    $cpt_names = array(); // array because some modules include multiple post types
-	
-	// Which post types are associated with this module? Build array
-	// Deal w/ modules whose names don't perfectly match their CPT names
-	if ( $module == "people" ) {
-		$sub_modules[] = "people";
-		$sub_modules[] = "groups";
-		$primary_cpt = "person";
-		$cpt_names[] = "person";
-		//$cpt_names[] = "group";
-	} else if ( $module == "places" ) {
-		$sub_modules[] = "venues";
-		$primary_cpt = "location";
-		$cpt_names[] = "location";
-		//$cpt_names[] = "building"; // address
-	} else if ( $module == "events" ) {
-		$sub_modules[] = "events";
-		$primary_cpt = "event";
-		$cpt_names[] = "event";
-		$cpt_names[] = "event_series";
-	} else {
-		$sub_modules[] = $module;
-		$cpt_name = $module;
-		// Make it singular -- remove trailing "s"
-		if ( substr($cpt_name, -1) == "s" && $cpt_name != "press" ) { $cpt_name = substr($cpt_name, 0, -1); }
-		$primary_cpt = $cpt_name;
-		$cpt_names[] = $cpt_name;
-	}
-	
-	// Load associated functions file, if any
-	foreach ( $sub_modules as $sub_module ) {
-		$filepath = WHX4_PLUGIN_DIR.'/modules/'.$sub_module.'.php';
-		$arr_exclusions = array ( 'addresses', 'buildings', 'organizations', 'ensembles' );
-		if ( !in_array( $module, $arr_exclusions) ) { // skip modules w/ no associated function files
-			if ( file_exists($filepath) ) { require( $filepath ); } else { echo "WHx4 module file $filepath not found"; }
-		}
-    }
-    
-	if ( function_exists('acf_add_options_page') ) {
-		// Add module options page
-    	acf_add_options_sub_page(array(
-			'page_title'	=> ucfirst($module).' Module Options',
-			'menu_title'    => ucfirst($module).' Module Options',//'menu_title'    => 'Archive Options', //ucfirst($cpt_name).
-			'menu_slug' 	=> $module.'-module-options',
-			'parent_slug'   => 'edit.php?post_type='.$primary_cpt,
-		));
-	}
-
-}
 
 /* +~+~+ Misc Functions +~+~+ */
 
@@ -203,11 +106,6 @@ function whx4_redirect() {
         //wp_redirect( site_url('/de/') ); 
         //exit; 
     }
-    
-    /*if (get_query_var('lang') == "de") {
-        wp_redirect( site_url('/de/') ); 
-        exit; 
-    }*/
 }
 
 ?>
