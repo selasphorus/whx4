352807ed9347d1d8bbe1be0092ff1eb2b664a34c
atc <birdhive@gmail.com>
2025-04-12 11:34:27 -0400

    Added missing interface

---
+++ src/Core/Contracts/ModuleInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace atc\WHx4\Core\Contracts;
+
+interface ModuleInterface
+{
+    public function getName(): string;
+
+    public function getPostTypeHandlers(): array;
+
+    public function boot(): void;
+}
