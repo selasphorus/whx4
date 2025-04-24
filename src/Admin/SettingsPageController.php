57de285cdd0644af40e9a8da81e6932612f43d12
atc <birdhive@gmail.com>
2025-04-24 15:51:14 -0400

    cont.

---
+++ src/Admin/SettingsPageController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace atc\WHx4\Admin;
+
+use atc\WHx4\Plugin;
+
+class SettingsPageController
+{
+    protected Plugin $plugin;
+
+    public function __construct( Plugin $plugin )
+    {
+        $this->plugin = $plugin;
+
+        //add_action( 'admin_menu', [ $this, 'addSettingsPage' ] );
+        //add_action( 'admin_init', [ $this, 'registerSettings' ] );
+    }
+
+    public function addHooks(): void
+    {
+        add_action( 'admin_menu', [ $this, 'addSettingsPage' ] );
+        add_action( 'admin_init', [ $this, 'registerSettings' ] );
+    }
+
+    public function addSettingsPage(): void
+    {
+        //error_log( '=== SettingsManager->addSettingsPage() ===' );
+        add_options_page(
+            'WHx4 v2 Plugin Settings', // page_title
+            'WHx4 v2 Settings', // menu_title
+            'manage_options', // capability
+            'whx4_settings', // menu_slug
+            [ $this, 'renderSettingsPage' ] // callback
+        );
+    }
+
+    public function registerSettings(): void
+    {
+        register_setting( 'whx4_plugin_settings_group', 'whx4_plugin_settings' );
+
+        add_settings_section(
+            'whx4_main_settings',
+            'Module and Post Type Settings',
+            null,
+            'whx4_plugin_settings'
+        );
+
+        // Add settings fields here
+        // Example:
+        // add_settings_field( ... );
+    }
+
+    public function renderSettingsPage(): void
+    {
+        $this->plugin->renderView( 'settings-page', [
+            'availableModules' => $this->plugin->getAvailableModules(),
+            'activeModules'    => $this->plugin->getSettingsManager()->getActiveModuleSlugs(),
+            'enabledPostTypes' => $this->plugin->getSettingsManager()->getEnabledPostTypeSlugs(),
+        ]);
+    }
+
+}
