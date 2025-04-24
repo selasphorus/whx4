d3204ece8ed7299aeec5c3c2579876cc16d69620
atc <birdhive@gmail.com>
2025-04-24 15:50:55 -0400

    cont.

--- src/Core/PostUtils.php
+++ src/Core/PostUtils.php
@@ -4,11 +4,13 @@ namespace atc\WHx4\Core;
 
 class PostUtils
 {
-    public static function say_hello() {
+    public static function say_hello()
+    {
         echo "<!-- Hello from Rex! -->";
     }
 
-    public static function merge_titles($title1, $title2) {
+    public static function merge_titles($title1, $title2)
+    {
         return $title1 . ' / ' . $title2;
     }
 }
