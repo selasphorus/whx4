e53bbf46e29aa7b556187dcc8a850c4e24dac804
atc <birdhive@gmail.com>
2025-04-25 18:02:05 -0400

    cleanup

--- src/Core/Traits/AppliesTitleArgs.php
+++ src/Core/Traits/AppliesTitleArgs.php
@@ -8,13 +8,6 @@ trait AppliesTitleArgs
 {
     protected function applyTitleArgs( string $postType, array $args ): void
     {
-        //error_log("=== trait AppliesTitleArgs: applyTitleArgs ===");
         TitleFilter::setGlobalArgsForPostType( $postType, $args );
     }
-    /*protected function applyTitleArgs( string $post_type, array $args ): void
-    {
-        if ( method_exists( $this, 'isPostTypeEnabled' ) && $this->isPostTypeEnabled( $post_type ) ) {
-            TitleFilter::setGlobalArgsForPostType( $post_type, $args );
-        }
-    }*/
 }
