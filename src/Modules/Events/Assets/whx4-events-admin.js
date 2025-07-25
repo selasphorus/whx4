4e546d1bdbd1386925ec5cea34df35eee4221ccc
atc <birdhive@gmail.com>
2025-07-25 14:44:16 -0400

    TS ajax

--- src/Modules/Events/Assets/whx4-events-admin.js
+++ src/Modules/Events/Assets/whx4-events-admin.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
      * Send a generic AJAX request to the backend with event instance data.
      */
     function sendAjax(action, postId, date, button) {
-        //alert('action: '+action+'; postId: '+postId+'; date: '+date+'; button: '+button);
+        alert('action: '+action+'; postId: '+postId+'; date: '+date+'; button: '+button);
         //
         button.disabled = true;
 
