223fe97dfdc0f20155d50e6f57a046e2d154f404
atc <birdhive@gmail.com>
2025-08-21 18:41:34 -0400

    cont.

--- src/Modules/People/PeopleModule.php
+++ src/Modules/People/PeopleModule.php
@@ -4,7 +4,7 @@ namespace atc\WHx4\Modules\People;
 
 use atc\WHx4\Core\Module as BaseModule;
 use atc\WHx4\Modules\People\PostTypes\Person;
-//use atc\WHx4\Modules\People\PostTypes\GroupEntity;
+use atc\WHx4\Modules\People\PostTypes\GroupEntity;
 //use atc\WHx4\Modules\People\PostTypes\Identity;
 
 final class PeopleModule extends BaseModule
