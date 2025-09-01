c6a9ecfbfb2cafa60d2b2129a9cb028d11fb8439
atc <birdhive@gmail.com>
2025-09-01 14:44:03 -0400

    name, not menu_name (?)

--- src/Modules/Logbook/PostTypes/LogEntry.php
+++ src/Modules/Logbook/PostTypes/LogEntry.php
@@ -15,7 +15,7 @@ class LogEntry extends PostTypeHandler
             //'capability_type' => ['secret','secrets'],
             //'hierarchical' => false,
             //'taxonomies' => ['admin_tag', 'secret_category'],
-            'menu_name' => 'Logbook',
+            'name' => 'Logbook',
         ];
 
         parent::__construct( $config, $post );
