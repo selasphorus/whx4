072a82a17ae504c2832cfe1a21e85904c46844e7
atc <birdhive@gmail.com>
2025-09-27 15:10:52 -0400

    cont. ts

--- src/Modules/Supernatural/Views/Monster/content.php
+++ src/Modules/Supernatural/Views/Monster/content.php
@@ -13,10 +13,14 @@ $sn = ($handler && method_exists($handler, 'getSN')) ? $handler->getSN() : '';
 Monster view test -- content (partial/appended).
 <?php echo "color: " . $color; ?><br />
 <?php echo "secret name: " . $sn; ?><br />
-<?php //echo "getPostID: " . $handler->getPostID($post); ?>
+<?php
+//echo "getPostID: " . $handler->getPostID($post);
+?>
 </div>
 
 <div class="troubleshootingg">
-<?php //echo 'post: <pre>' . print_r($post,true) . '</pre>'; // ok ?>
-<?php //echo 'handler: <pre>' . print_r($handler,true) . '</pre>'; // ok ?>
+<?php
+echo 'post: <pre>' . print_r($post,true) . '</pre>'; // ok
+echo 'handler: <pre>' . print_r($handler,true) . '</pre>'; // ok
+?>
 </div>
