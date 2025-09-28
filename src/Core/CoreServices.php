db2785c311c7af1b23c895c66ff00dc5795424e7
atc-mba25 <birdhive@gmail.com>
2025-09-28 13:10:10 -0400

    capital H

--- src/Core/CoreServices.php
+++ src/Core/CoreServices.php
@@ -5,7 +5,7 @@ namespace atc\WHx4\Core;
 use atc\WHx4\Utils\TitleFilter;
 //use atc\WHx4\Core\FieldGroupLoader;
 use atc\WHx4\Core\TemplateRouter;
-use atc\Whx4\Core\Shortcodes\ShortcodeManager;
+use atc\WHx4\Core\Shortcodes\ShortcodeManager;
 
 class CoreServices
 {
