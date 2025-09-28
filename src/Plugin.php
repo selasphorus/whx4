a333db0273534ab2af69c749f9d89a924e6702f2
atc-mba25 <birdhive@gmail.com>
2025-09-28 13:49:12 -0400

    TS order of events -- shortcodes not being registreed properly: git push

--- src/Plugin.php
+++ src/Plugin.php
@@ -104,6 +104,7 @@ final class Plugin implements PluginContext
 
         // Run as early as possible on init so modules are ready before init:10 work.
 		if ( did_action('init') ) {
+		    error_log( 'Already did init; finishBoot now.' );
 			$this->finishBoot(); // if we're already past init (rare), just run now
 		} else {
 			add_action('init', [$this, 'finishBoot'], 0);
