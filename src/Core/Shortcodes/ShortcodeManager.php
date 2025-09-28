e70c1d10dfee0f5bbf0f71c038f21ef5cd5b839c
atc-mba25 <birdhive@gmail.com>
2025-09-28 13:44:22 -0400

    cont. ts

--- src/Core/Shortcodes/ShortcodeManager.php
+++ src/Core/Shortcodes/ShortcodeManager.php
@@ -18,7 +18,7 @@ final class ShortcodeManager
         add_action('init', [self::class, 'registerAll'], 99);
     }
 
-    public static function registerAll(): void
+    /*public static function registerAll(): void
     {
         error_log('=== ShortcodeManager::registerAll() ===');
 
@@ -34,5 +34,37 @@ final class ShortcodeManager
             $instance = new $fqcn();
             add_shortcode($fqcn::tag(), [$instance, 'handle']);
         }
-    }
+    }*/
+    
+    public static function registerAll(): void
+	{
+		error_log('=== ShortcodeManager::registerAll() ===');
+	
+		$candidates = (array) apply_filters('whx4_register_shortcodes', []);
+		error_log('Shortcode candidates: ' . json_encode($candidates, JSON_UNESCAPED_SLASHES));
+	
+		$classes = [];
+		foreach ($candidates as $fqcn) {
+			if (!is_string($fqcn)) {
+				error_log('Rejected: non-string value from whx4_register_shortcodes');
+				continue;
+			}
+			if (!class_exists($fqcn)) {
+				error_log("Rejected: class does not exist: {$fqcn}");
+				continue;
+			}
+			if (!is_subclass_of($fqcn, \atc\WHx4\Core\Contracts\ShortcodeInterface::class)) {
+				error_log("Rejected: {$fqcn} does not implement ShortcodeInterface");
+				continue;
+			}
+			$classes[] = $fqcn;
+		}
+	
+		foreach ($classes as $fqcn) {
+			error_log('Registering shortcode class: ' . $fqcn);
+			$instance = new $fqcn();
+			add_shortcode($fqcn::tag(), [$instance, 'handle']);
+		}
+	}
+
 }
