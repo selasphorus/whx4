e30dac6890e253163b97de622789635f6a8111c8
atc-mba25 <birdhive@gmail.com>
2025-10-04 18:44:57 -0400

    cleanup, ts

--- src/Modules/Supernatural/Shortcodes/SupernaturalShortcode.php
+++ src/Modules/Supernatural/Shortcodes/SupernaturalShortcode.php
@@ -15,25 +15,20 @@ final class SupernaturalShortcode implements ShortcodeInterface
     }
 
     public function render(array $atts = [], string $content = '', string $tag = ''): string
-    //public function render(array $atts = [], ?string $content = null): string
     {
         $ctx = WHx4::ctx();
-        $key    = ClassInfo::getModuleKey(self::class);   // 'supernatural'
+        $key = ClassInfo::getModuleKey(self::class); // 'supernatural'
         $module = $ctx->getModule($key);
-        //$module = $ctx->getModule('supernatural'); // get active module instance
 
         if (!$module) {
             return '<p>Supernatural module inactive.</p>';
         }
 
-        //$name  = $module->getRandomCreatureName();
         $stats = $module->getModuleStats();
 
         ob_start(); ?>
-        <div class="rex-supernatural">
-            <!--p><strong>Random Creature:</strong> <?= //esc_html($name) ?></p-->
-            <p><strong>Monsters:</strong> <?= (int)$stats['monsters'] ?></p>
-            <!--p><strong>Enchanters:</strong> <?= //(int)$stats['enchanters'] ?></p-->
+        <div class="whx4-supernatural">
+            <p><strong>Monsters:</strong> <?php echo (int)$stats['monsters']; ?></p>
         </div>
         <?php
         return ob_get_clean();
