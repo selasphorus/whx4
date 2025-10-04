487aac162c1bb4a078b9d939da03fa2955c589ef
atc-mba25 <birdhive@gmail.com>
2025-10-04 19:58:44 -0400

    Testing a new module-level method

--- src/Modules/Supernatural/Views/module-view-test.php
+++ src/Modules/Supernatural/Views/module-view-test.php
@@ -1,3 +1,4 @@
 <div class="whx4-supernatural">
-	<p><strong>Monsters:</strong> <?php echo (int)$stats['monsters']; ?></p>
+	<p><strong>How many Monsters?:</strong> <?php echo (int)$stats['monsters']; ?></p>
+	<p><strong>Blue Monsters:</strong> <?php echo print_r($blueMonsterPosts, true); ?></p>
 </div>
