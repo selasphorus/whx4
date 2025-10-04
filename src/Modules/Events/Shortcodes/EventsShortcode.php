9f1e1d8a1633f81f7ed546a81b433c3be8fcc3e8
atc <birdhive@gmail.com>
2025-10-03 23:32:04 -0400

    cont.

--- src/Modules/Events/Shortcodes/EventsShortcode.php
+++ src/Modules/Events/Shortcodes/EventsShortcode.php
@@ -41,7 +41,7 @@ final class EventsShortcode implements ShortcodeInterface
         $info .= "[" . $result['pagination']['found'] . "] posts found<br />";
         //$info .= "posts: <pre>" . print_r($posts, true) . "</pre>";
         //$info .= "atts: <pre>" . print_r($atts, true) . "</pre>";
-        $info .= "wp_args: <pre>" . print_r($result['debug']['args'], true) . "</pre>";
+        //$info .= "wp_args: <pre>" . print_r($result['debug']['args'], true) . "</pre>";
         //$info .= "query_request: <pre>" . $result['debug']['query_request'] . "</pre>";
 
         // Handler factory so views can call CPT methods safely.
