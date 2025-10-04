a28c9238b0b79865c705357a9578dae16f33145f
atc-mba25 <birdhive@gmail.com>
2025-10-04 15:08:45 -0400

    New trait to facilitate subtype queries

---
+++ src/Core/Traits/SubtypeQueryHelpers.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace atc\WHx4\Core\Traits;
+
+use atc\WHx4\Core\Query\PostQuery;
+
+trait SubtypeQueryHelpers
+{
+    /**
+     * Convenience method: run the query and return WP_Post[].
+     * @return \WP_Post[]
+     */
+    public function find(array $overrides = []): array
+    {
+        return PostQuery::find($this->getQuerySpec($overrides));
+        //$result = Event::find($atts);
+    }
+}
