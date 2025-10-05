a0fbfbc9883c8726cf756eca754ba2405bcbf22a
atc-mba25 <birdhive@gmail.com>
2025-10-04 20:43:15 -0400

    cont. ts

--- src/Modules/Supernatural/Shortcodes/SupernaturalShortcode.php
+++ src/Modules/Supernatural/Shortcodes/SupernaturalShortcode.php
@@ -25,11 +25,23 @@ final class SupernaturalShortcode implements ShortcodeInterface
         }
 
         $stats = $module->getModuleStats();
-        $blueMonsterPosts  = $module->findMonstersByColor('blue') ?? [];
+        $monsters = $module->findMonstersByColor('blue') ?? [];
+        //$result = Event::find($atts);
+        $monsterPosts  = $monsters['posts'] ?? [];
 
-        
+        // Pagination info for the view.
+        $pagination = $monsters['pagination'] ?? ['found' => 0, 'max_pages' => 0, 'paged' => 1];
+
+        // Troubleshooting info
+        $info .= "[" . $monsters['pagination']['found'] . "] posts found<br />";
+        //$info .= "posts: <pre>" . print_r($posts, true) . "</pre>";
+        //$info .= "atts: <pre>" . print_r($atts, true) . "</pre>";
+        //$info .= "wp_args: <pre>" . print_r($result['debug']['args'], true) . "</pre>";
+        //$info .= "query_request: <pre>" . $result['debug']['query_request'] . "</pre>";
+
         // Handler factory so views can call CPT methods safely.
-        //$handlerFactory = [PostTypeHandler::class, 'getHandlerForPost'];
+        $handlerFactory = [PostTypeHandler::class, 'getHandlerForPost'];
+
 
         // Choose a view variant (list|grid|table); fall back to list.
         //$viewVariant = in_array($atts['view'], ['list', 'grid', 'table'], true) ? $atts['view'] : 'list';
@@ -38,15 +50,15 @@ final class SupernaturalShortcode implements ShortcodeInterface
         $view = "module-view-test";
         
         $vars = [
-            //'posts'      => $posts,
-            //'handler'    => $handlerFactory,
+            'posts'      => $monsterPosts,
+            'handler'    => $handlerFactory,
             //'atts'       => $atts,
             //'pagination' => $pagination,
             'stats' => $stats,
-            'blueMonsterPosts' => $blueMonsterPosts,
+            //'blueMonsterPosts' => $blueMonsterPosts,
             //'info' => $info, // for TS -- deprecate in favor of:
             // Optionally pass debug through when WHX4_DEBUG is on:
-            'debug'      => $result['debug'] ?? null,
+            'debug'      => $monsters['debug'] ?? null,
         ];
 
         return ViewLoader::renderToString(
--- src/Modules/Supernatural/Views/module-view-test.php
+++ src/Modules/Supernatural/Views/module-view-test.php
@@ -1,5 +1,5 @@
 <div class="whx4-supernatural">
 	<p><strong>How many Monsters?:</strong> <?php echo (int)$stats['monsters']; ?></p>
 	<p><strong>Debug:</strong> <pre><?php echo print_r($debug, true); ?></pre></p>
-	<p><strong>Blue Monsters:</strong> <pre><?php echo print_r($blueMonsterPosts, true); ?></pre></p>
+	<p><strong>Blue Monsters:</strong> <pre><?php echo print_r($posts, true); ?></pre></p>
 </div>
