4b3b4ea64c8745adcbb7de17b0383f653b8e1aca
atc-mba25 <birdhive@gmail.com>
2025-10-04 20:12:11 -0400

    better formatting

--- src/Modules/Supernatural/Views/module-view-test.php
+++ src/Modules/Supernatural/Views/module-view-test.php
@@ -1,4 +1,4 @@
 <div class="whx4-supernatural">
 	<p><strong>How many Monsters?:</strong> <?php echo (int)$stats['monsters']; ?></p>
-	<p><strong>Blue Monsters:</strong> <?php echo print_r($blueMonsterPosts, true); ?></p>
+	<p><strong>Blue Monsters:</strong> <pre><?php echo print_r($blueMonsterPosts, true); ?></pre></p>
 </div>
