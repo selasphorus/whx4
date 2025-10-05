be1b2c35a3cd92f531bfc03a3eb40476f2ea773f
atc-mba25 <birdhive@gmail.com>
2025-10-04 21:55:23 -0400

    cont. ts

--- src/Modules/Supernatural/Views/module-view-test.php
+++ src/Modules/Supernatural/Views/module-view-test.php
@@ -1,10 +1,10 @@
 <div class="whx4-supernatural">
 	<p><strong>How many Monsters?:</strong> <?php echo (int)$stats['monsters']; ?></p>
-	<p><strong>Debug:</strong> <pre><?php //echo print_r($debug, true); ?></pre></p>
 	<p><strong>Blue Monsters:</strong> <pre><?php //echo print_r($posts, true); ?></pre></p>
 	<?php 
 	foreach ($posts as $post) {
 	    echo $post->post_title."<br />";
 	}
 	?>
+	<p><strong>Debug:</strong> <pre><?php echo print_r($debug, true); ?></pre></p>
 </div>
