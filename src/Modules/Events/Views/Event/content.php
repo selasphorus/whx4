f1ac62b4daea3ce088695b6167f5632ea6d216d7
atc-mba25 <birdhive@gmail.com>
2026-01-22 16:40:57 -0500

    Mods to fine-tune display for each type of event

--- src/Modules/Events/Views/Event/content.php
+++ src/Modules/Events/Views/Event/content.php
@@ -16,14 +16,20 @@ if (!defined('ABSPATH')) {
 }
 ?>
 
-<div class="event-view">
-    <div class="event-summary">
-        <p><strong>Event Start Date:</strong> <?php echo esc_html($startDate); ?></p>
-        <!--p><strong>Total Instances on Record:</strong> <?php //echo $viewData['total_count']; ?></p-->
-    </div>
-    
-    <!-- Event instances (pre-rendered nested view) -->
-    <?php echo $instancesListHtml; ?>
+<div class="event-view">   
+    <?php
+    // If this is a recurring event, list the instance(s)
+    if ( $instancesListHtml ):
+        echo $instancesListHtml; // Event instances (pre-rendered nested view)
+    else:
+    // If it's NOT a recurring event, just show the event start date
+    ?>
+		<div class="event-summary">
+			<p><strong>Event Start Date:</strong> <?php echo esc_html($startDate); ?></p>
+		</div>
+    <?php
+    endif;
+    ?>
     
     <hr class="debug-divider" />
     <details class="debug-info">
--- src/Modules/Events/Views/event-instances-list.php
+++ src/Modules/Events/Views/event-instances-list.php
@@ -1,24 +1,29 @@
 <?php
 // Check to see if we're dealing with an individual instance of a recurring event
 $event_instance = get_query_var('event_instance');
-if ( $event_instance ) {
-    echo 'event_instance: ' . $event_instance . "<br />";
-}
-?>
-
-<h3>Event Instances</h3>
-
-<div class="whx4-event-instances-list">
-<!-- event-instances-list.php -->
-<?php foreach ( $instances as $instance ): ?>
-    <div class="event-instance">
-        <a href="<?php echo esc_url( $instance['permalink'] ); ?>" target="_blank">
-            <?php echo esc_html( $instance['datetime']->format( 'F j, Y' ) ); ?>
-        </a>
-        
-        <?php if ( $instance['is_override'] ): ?>
-            <span class="badge">Rescheduled</span>
-        <?php endif; ?>
+if ( $event_instance ):
+    //echo 'event_instance: ' . $event_instance . "<br />";
+    ?>
+    <div class="event-summary recurrence-instance">
+        <p><strong>Event Date:</strong> <?php echo $event_instance; ?></p>
     </div>
-<?php endforeach; ?>
-</div> <!-- close .whx4-event-instances-list -->
+    <?php
+else:
+// Otherwise, list all instances
+?>
+	<h3>Event Instances</h3>
+	<div class="whx4-event-instances-list">
+	<!-- event-instances-list.php -->
+	<?php foreach ( $instances as $instance ): ?>
+		<div class="event-instance">
+			<a href="<?php echo esc_url( $instance['permalink'] ); ?>" target="_blank">
+				<?php echo esc_html( $instance['datetime']->format( 'F j, Y' ) ); ?>
+			</a>
+			
+			<?php if ( $instance['is_override'] ): ?>
+				<span class="badge">Rescheduled</span>
+			<?php endif; ?>
+		</div>
+	<?php endforeach; ?>
+	</div> <!-- close .whx4-event-instances-list -->
+<?php endif; ?>
