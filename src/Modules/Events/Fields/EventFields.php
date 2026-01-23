61a142278238e77a25e5af04db1617a48417a698
atc-mba25 <birdhive@gmail.com>
2026-01-23 17:12:01 -0500

    Prep to add series field and migrate keys

--- src/Modules/Events/Fields/EventFields.php
+++ src/Modules/Events/Fields/EventFields.php
@@ -10,7 +10,26 @@ final class EventFields implements FieldGroupInterface
     {
         //error_log( 'EventFields::register' );
         if ( !function_exists('acf_add_local_field_group') ) return;
-
+        
+        //use atc\WHx4\Migrations\FieldKeyMigrator;
+        /*
+        // Migrate
+        FieldKeyMigrator::migrate([
+            'field_624775f4b6221' => [
+                'new_field_key' => 'field_whx4_modulename_first_name',
+                'old_meta_key'  => 'first_name',
+                'new_meta_key'  => 'whx4_modulename_first_name',
+            ],
+            'field_abc123xyz456' => [
+                'new_field_key' => 'field_whx4_notes',
+                // no meta_key rename
+            ],
+        ]);
+        */
+        
+        // TODO: add series field
+        
+        
         acf_add_local_field_group([
             'key'    => 'group_event_fields',
             'title'  => 'WHx4 Event Details',
