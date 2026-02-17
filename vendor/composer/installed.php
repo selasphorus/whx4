2a60d680161827b2814a4e4fd48f3243aa960b07
atc-mba25 <birdhive@gmail.com>
2026-02-17 14:55:03 -0500

    updated vendor files

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,26 +3,26 @@
         'name' => 'atc/whx4',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '0f7d7d52c152b4e0520a6ab58650b1b7920d1164',
+        'reference' => '82e90bb95c9ef172f152c8d5fe92c9693eb0ca1f',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'dev' => false,
+        'dev' => true,
     ),
     'versions' => array(
         'atc/whx4' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '0f7d7d52c152b4e0520a6ab58650b1b7920d1164',
+            'reference' => '82e90bb95c9ef172f152c8d5fe92c9693eb0ca1f',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'composer/installers' => array(
-            'pretty_version' => 'v1.12.0',
-            'version' => '1.12.0.0',
-            'reference' => 'd20a64ed3c94748397ff5973488761b22f6d3f19',
+            'pretty_version' => 'v2.3.0',
+            'version' => '2.3.0.0',
+            'reference' => '12fb2dfe5e16183de69e784a7b84046c43d97e8e',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/./installers',
             'aliases' => array(),
@@ -36,18 +36,6 @@
             'install_path' => __DIR__ . '/../rlanvin/php-rrule',
             'aliases' => array(),
             'dev_requirement' => false,
-        ),
-        'roundcube/plugin-installer' => array(
-            'dev_requirement' => false,
-            'replaced' => array(
-                0 => '*',
-            ),
-        ),
-        'shama/baton' => array(
-            'dev_requirement' => false,
-            'replaced' => array(
-                0 => '*',
-            ),
         ),
     ),
 );
