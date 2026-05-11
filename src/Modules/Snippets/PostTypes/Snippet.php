dcdd486e9e0117ce566f4158a17bd30cdad2e62f
atc-mba25 <birdhive@gmail.com>
2026-05-11 16:27:27 -0400

    Added new mobile_position field to match main branch; minor cleanup on CPT class

--- src/Modules/Snippets/PostTypes/Snippet.php
+++ src/Modules/Snippets/PostTypes/Snippet.php
@@ -19,39 +19,5 @@ class Snippet extends PostTypeHandler
 	{
 	    parent::boot(); // Optional if you add shared logic later
 	}
-
-	public function getPersonDates(?\WP_Post $post = null, $styled = false): string
-	{
-		$info = ""; // init
-
-        $p = $post ?? $this->getPost();
-        if ( empty($p) ) { return "no post found"; } // $info
-        $pID = $p->ID;
-
-		// Try ACF get_field instead?
-		$birth_year = get_post_meta( $pID, 'birth_year', true );
-		$death_year = get_post_meta( $pID, 'death_year', true );
-		$dates = get_post_meta( $pID, 'dates', true );
-
-		if ( !empty($birth_year) && !empty($death_year) ) {
-			$info .= "(".$birth_year."-".$death_year.")";
-		} else if ( !empty($birth_year) ) {
-			$info .= "(b. ".$birth_year.")";
-		} else if ( !empty($death_year) ) {
-			$info .= "(d. ".$death_year.")";
-		} else if ( !empty($dates) ) {
-			$info .= "(".$dates.")";
-		}
-
-		if ( !empty($info) ) {
-			if ( $styled ) {
-				$info = '<span class="person_dates">&nbsp;'.$info.'</span>';
-			} else {
-				$info = ' '.$info; // add space before dates str
-			}
-		}
-
-		return $info;
-	}
 }
 
